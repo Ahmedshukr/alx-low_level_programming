e7c795ff62ce4249fa38de2bd950555ddd5759af
Ahmedshukr <ahmetoshukr@gmail.com>
2022-10-13 11:26:15 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Here is the description: this code prints the phrase inside the puts function
+ * Return 0 if the code is compiled successfully
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
