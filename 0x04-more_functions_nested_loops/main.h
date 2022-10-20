d9631d470ed52c2b73ff1b4787195e2007a9f9c4
Ahmedshukr <ahmetoshukr@gmail.com>
2022-10-20 03:20:03 -0700

    the main header

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for function prototypes
+ * void prototype (void)
+ * int prototype (int)
+ */
+int _isupper(int);
+
+#endif /* MAIN_H */
