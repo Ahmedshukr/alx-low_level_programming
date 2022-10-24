54136ce031bb320aab04adaf6d59e314118491c4
Ahmedshukr <ahmetoshukr@gmail.com>
2022-10-24 07:57:09 -0700

    print string in the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * *@str: A pointer to an int that will be changed
+ *
+ * Return: void which means our answer is correct
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		_putchar (str[i]);
+	}
+	_putchar('\n');
+	return;
+}
