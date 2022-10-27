91dd42895e9534d12b90d5bef4d43a3799d2acdb
Ahmedshukr <ahmetoshukr@gmail.com>
2022-10-27 05:54:38 -0700

    copying string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: pointer of the destination
  * @src: pointer of the source
  * @n: number of bytes
+ * Return: returns a pointer to the resulting string dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
