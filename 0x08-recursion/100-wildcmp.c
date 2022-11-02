f1e7eeb58aefffb172051d3b6803161ce06447c6
Ahmedshukr <ahmetoshukr@gmail.com>
2022-11-02 05:18:31 -0700

    wildcmp

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * wildcmp - 
+ * @s1: the input argument
+ * @s2: input argumnet
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+
+{
+
+	return (0);
+
+}
