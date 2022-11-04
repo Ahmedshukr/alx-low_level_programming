4076653229b66516f9ebbaf6b983c9d3a8833c8a
Ahmedshukr <ahmetoshukr@gmail.com>
2022-11-04 11:04:04 -0700

    string compare

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: returns the compare result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+		;
+	return (s1[i] - s2[i]);
+}
