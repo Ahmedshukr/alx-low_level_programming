224e7a5b86ffd3087397803d6709b680fbec7803
Ahmedshukr <ahmetoshukr@gmail.com>
2022-11-04 11:02:28 -0700

    string chr

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - search a character in a string
+ * @s: a pointer to the string
+ * @c: character to be searched
+ * Return: always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
