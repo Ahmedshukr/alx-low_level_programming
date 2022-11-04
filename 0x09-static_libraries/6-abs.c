593f412f646a7b8e3cb8f018315aa5e67e39f8b2
Ahmedshukr <ahmetoshukr@gmail.com>
2022-11-04 11:05:32 -0700

    abs

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - this returns the absolute value of an input
+ * @n: Contains the value
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (n * (-1));
+	}
+	else if (n == 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (n);
+	}
+}
