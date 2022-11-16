a7b4ed7ec5c42865ed4d9b551ed58459af5fa974
Ahmedshukr <ahmetoshukr@gmail.com>
2022-11-16 05:06:02 -0800

    contain the function that selects the correct function to perform the operation asked by the user.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,8 +4,6 @@
 /**
  * get_op_func - Selects the correct function to perform the operation
  * @s: The operator passed as argument.
- *
-
  * Return: A pointer to the function corresponding
 
  *         to the operator given as a parameter.
