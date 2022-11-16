63dad434e0b9d8ed3f567f96315a8a7edf0800ac
Ahmedshukr <ahmetoshukr@gmail.com>
2022-11-16 05:05:24 -0800

    contain the function that selects the correct function to perform the operation asked by the user.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - Selects the correct function to perform
-
- *               the operation asked by the user.
-
+ * get_op_func - Selects the correct function to perform the operation
  * @s: The operator passed as argument.
 
  *
